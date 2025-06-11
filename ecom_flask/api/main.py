dc0b40c876836f628ad9ca2c6956479da455ad98
Kevin-CNC <kevincncaplescu@gmail.com>
2025-06-11 17:47:03 +0100

    Improved frontend

--- ecom_flask/api/main.py
+++ ecom_flask/api/main.py
@@ -1,10 +1,26 @@
 # Main API logic script; here create your endpoints with routes and return values.
 from flask import render_template, request, Blueprint, url_for, jsonify
 import flask_login
+import json
+from dotenv import load_dotenv
+import os
 
-apiBlueprint = Blueprint("main", __name__)
+# Load up environment variables for configuration
+load_dotenv()
+
+apiBlueprint = Blueprint("api", __name__)
 
 
-@apiBlueprint.route("/get-product-list/<apiKey>")
+# Example of a GET request
+@apiBlueprint.route("/get-product-list/<apiKey>", methods=["GET"])
 def getListOfProducts(apiKey):
-    return jsonify("Skibidi Toilet!!!!!!!!")
+    
+    products_path = os.path.join(os.path.dirname(__file__), "products.json")
+    
+    with open(products_path, "r") as f:
+        products_data = json.load(f)
+        
+    return jsonify(products_data)
+
+
+getListOfProducts('test-key')
--- ecom_flask/errors/routes.py
+++ ecom_flask/errors/routes.py
@@ -1,7 +1,7 @@
 # Main Blueprint for error handling; Will just handle the error code and display the appropriate message
 from flask import render_template, request, Blueprint
 
-errorRoutesBlueprint = Blueprint("main", __name__)
+errorRoutesBlueprint = Blueprint("errors", __name__)
 
 @errorRoutesBlueprint.errorhandler(400)
 @errorRoutesBlueprint.errorhandler(403)
